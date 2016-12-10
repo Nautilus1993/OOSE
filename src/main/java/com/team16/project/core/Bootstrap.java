38280e1958c5305769b95aad4b5230858d3367c9
Mengdan Zhang <mzhang45@jhu.edu>
2016-12-10 18:16:30 -0500

    no message

--- src/main/java/com/team16/project/core/Bootstrap.java
+++ src/main/java/com/team16/project/core/Bootstrap.java
@@ -5,19 +5,26 @@ import com.team16.project.Image.UserPhotoController;
 import com.team16.project.Item.ItemDetailController;
 import com.team16.project.ItemList.ItemListController;
 import com.team16.project.Login.LoginController;
+import com.team16.project.Model.MyAccountDB;
 import com.team16.project.MyAccount.MyAccountController;
 import com.team16.project.MyAccount.MyAccountService;
+import com.team16.project.MyList.SellingList.SellingListController;
+import com.team16.project.MyList.SellingList.SellingListService;
 import com.team16.project.MyList.WishList.WishController;
 import com.team16.project.registration.email.EmailRegistrationController;
 import com.team16.project.registration.password.PasswordController;
 import com.team16.project.registration.phone.PhoneRegistrationController;
-import com.team16.project.Model.MyAccountDB;
 import com.team16.project.subscribe.SubscribeSchedule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
+//import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+
 public class Bootstrap {
     public static final String DATABASE = "jdbc:sqlite:ProjectDB.db";
+    private static final Logger LOGGER = LoggerFactory.getLogger(Bootstrap.class);
 
     public static void main(String[] args) throws SQLException {
         new EmailRegistrationController();
@@ -25,6 +32,7 @@ public class Bootstrap {
         new PasswordController();
         new LoginController();
         new ItemDetailController();
+        new UserPhotoController();
         new ItemListController();
         new WishController();
         new UserPhotoController();
@@ -35,5 +43,12 @@ public class Bootstrap {
         new MyAccountController(model);
         
         SubscribeSchedule subscribeSchedule = new SubscribeSchedule();
+
+        try {
+            SellingListService ser = new SellingListService();
+            new SellingListController(ser);
+        } catch (SellingListService.SellingListServiceException ex) {
+            LOGGER.error("Failed to create a SellingListController instance. Aborting");
+        }
     }
 }
