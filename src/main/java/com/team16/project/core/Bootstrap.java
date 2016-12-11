da97bcb3f4e8e5b02d1391bdedbe05aa54e50c6c
dachen <2672921697@qq.com>
2016-12-11 15:03:33 -0500

    abc

--- src/main/java/com/team16/project/core/Bootstrap.java
+++ src/main/java/com/team16/project/core/Bootstrap.java
@@ -14,7 +14,6 @@ import com.team16.project.MyList.WishList.WishController;
 import com.team16.project.registration.email.EmailRegistrationController;
 import com.team16.project.registration.password.PasswordController;
 import com.team16.project.registration.phone.PhoneRegistrationController;
-import com.team16.project.subscribe.SubscribeSchedule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,7 +41,7 @@ public class Bootstrap {
         MyAccountService model = new MyAccountService(projectDB);
         new MyAccountController(model);
         
-        SubscribeSchedule subscribeSchedule = new SubscribeSchedule();
+        //SubscribeSchedule subscribeSchedule = new SubscribeSchedule();
 
         try {
             SellingListService ser = new SellingListService();
