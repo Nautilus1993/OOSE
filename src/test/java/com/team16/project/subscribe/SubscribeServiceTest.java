3cf243618562318e7f5c6b187b8fc645ec7c7507
kyo744 <bwang48@jhu.edu>
2016-12-11 15:15:06 -0500

    Add files via upload

--- src/test/java/com/team16/project/subscribe/SubscribeServiceTest.java
+++ src/test/java/com/team16/project/subscribe/SubscribeServiceTest.java
@@ -1,5 +1,6 @@
 package com.team16.project.subscribe;
 
+import org.json.simple.parser.ParseException;
 import org.junit.Test;
 
 import java.sql.SQLException;
@@ -10,14 +11,22 @@ import static org.junit.Assert.*;
  * Created by kyo74_000 on 2016/12/10.
  */
 public class SubscribeServiceTest {
-    SubscribeService subscribeService = new SubscribeService();
-    int result = subscribeService.listUser();
+    @Test
+    public void subscribe() throws Exception {
 
-    public SubscribeServiceTest() throws SQLException {
+    }
+
+    SubscribeService subscribeService = new SubscribeService();
+
+
+    public SubscribeServiceTest() throws SQLException, ParseException {
+        int result = subscribeService.subscribe("{\"userId\":\"1\", \"itemId\":\"1\"}");
+        assertEquals(1, result);
     }
 
     @Test
     public void listUser() throws Exception {
+        int result = subscribeService.listUser();
         assertEquals(1, result);
     }
 
