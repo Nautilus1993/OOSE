88b81f52513790635695844b712afc466c457b71
kyo744 <bwang48@jhu.edu>
2016-12-21 16:09:02 -0500

    Update PhoneRegistrationCode.java

--- src/main/java/com/team16/project/registration/phone/PhoneRegistrationCode.java
+++ src/main/java/com/team16/project/registration/phone/PhoneRegistrationCode.java
@@ -3,7 +3,10 @@ package com.team16.project.registration.phone;
 import com.team16.project.registration.SecurityCode.RegistrationCodeSender;
 
 import javax.mail.MessagingException;
-
+/**
+ * Whenever an instance of this class is created, a text message containing registration code will be sent to user.
+ * @author OOSE_Team16
+ */
 class PhoneRegistrationCode extends RegistrationCodeSender{
     private static final String SUBJECT = "Phone Registration Code";
     private static final String TEXT = "Your phone registration code is ";
