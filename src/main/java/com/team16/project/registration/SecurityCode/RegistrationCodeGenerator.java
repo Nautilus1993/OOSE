4f3d0e5dcb64ae2e8d730d4b5331b9b46ef0f616
kyo744 <bwang48@jhu.edu>
2016-12-21 16:01:10 -0500

    Update RegistrationCodeGenerator.java

--- src/main/java/com/team16/project/registration/SecurityCode/RegistrationCodeGenerator.java
+++ src/main/java/com/team16/project/registration/SecurityCode/RegistrationCodeGenerator.java
@@ -1,7 +1,10 @@
 package com.team16.project.registration.SecurityCode;
 
 import java.util.Random;
-
+/**
+ * This class is a random number generating class used for creating registration code.
+ * @author OOSE_Team16
+ */
 public class RegistrationCodeGenerator {
     private static final int MIN = 100000;
     private static final int MAX = 999999;
