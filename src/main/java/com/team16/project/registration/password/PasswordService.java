b408305a0dfec30e228fecb78b431d35bab7afa8
kyo744 <bwang48@jhu.edu>
2016-12-21 16:08:04 -0500

    Update PasswordService.java

--- src/main/java/com/team16/project/registration/password/PasswordService.java
+++ src/main/java/com/team16/project/registration/password/PasswordService.java
@@ -5,7 +5,10 @@ import org.json.simple.parser.ParseException;
 
 import javax.mail.MessagingException;
 import java.sql.SQLException;
-
+/**
+ * This class is responsible for registering username and password
+ * @author OOSE_Team16
+ */
 public class PasswordService {
     private PasswordDB passwordDB;
 
@@ -13,7 +16,7 @@ public class PasswordService {
         passwordDB = new PasswordDB();
     }
 
-
+    // This method will creates a new user with the input user name and password in the body.
     int createUser(String body) throws ParseException, SQLException, MessagingException {
         return passwordDB.insertUser(body);
     }
