afd0fb2bfe4eff399a7857a2e872e0015589cb70
kyo744 <bwang48@jhu.edu>
2016-12-21 16:03:26 -0500

    Update EmailRegistrationCode.java

--- src/main/java/com/team16/project/registration/email/EmailRegistrationCode.java
+++ src/main/java/com/team16/project/registration/email/EmailRegistrationCode.java
@@ -2,7 +2,10 @@ package com.team16.project.registration.email;
 import com.team16.project.registration.SecurityCode.RegistrationCodeSender;
 
 import javax.mail.*;
-
+/**
+ * Whenever a new instance of this class is created, an email containing registration code will be sent to user.
+ * @author OOSE_Team16
+ */
 public class EmailRegistrationCode extends RegistrationCodeSender {
     private static final String SUBJECT = "Email Registration Code";
     private static final String TEXT = "Your email registration code is ";
